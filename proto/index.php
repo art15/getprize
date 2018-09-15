927dbed3552a820dd8def6cb1b1bf2aa3e79cf06
art15 <art15@users.noreply.github.com>
2018-09-15 16:18:50 +0600

    +add ajax-loader to markup

--- proto/index.php
+++ proto/index.php
@@ -12,8 +12,13 @@
     <title>Test proto "Get a prize"</title>
     <link href="/css/bootstrap.min.css" rel="stylesheet" />
     <link href="/css/fa/css/all.min.css" rel="stylesheet" />
+    <link href="/css/custom.css" rel="stylesheet" />
 </head>
 <body>
+    <div id="wait-please" class="wait-please">
+        <div class="loader"><i class="fa fa-spinner fa-3x fa-pulse"></i></div>
+        <div class="overlay"></div>
+    </div>
     <center>
 <?php
 if(!empty($_SESSION['auth_user'])){ //если прошли авторизацию
